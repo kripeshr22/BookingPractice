a20f8487fa1bba956109e16c011837429f436e7f
kripeshr22 <kranabha@students.pitzer.edu>
2021-05-16 23:48:47 -0700

    html css work

--- booking/urls.py
+++ booking/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import RoomList, BookingList, BookingView
+from .views import RoomListView, BookingList, RoomDetailView, BookingView
 
 # Declare our app names
 APP_NAME = 'hotel'
 
 urlpatterns = [
-    path('roomlist/', RoomList.as_view(), name='RoomList'),
+    path('roomlist/', RoomListView, name='RoomList'),
     path('booking_list/', BookingList.as_view(), name='BookingList'),
-    path('bookview/', BookingView.as_view(), name='BookingView')
-]
+    path('bookview/', BookingView.as_view(), name='BookingView'),
+    path('room/<category>', RoomDetailView.as_view(), name='RoomDetailView'),]
--- booking/views.py
+++ booking/views.py
@@ -1,16 +1,71 @@
 from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView, FormView
+from django.views.generic import ListView, FormView, View
+from django.urls import reverse
 from booking.booking_functions.available import check_available
 from .models import Room, Booking
 from .forms import AvailableForm
 
 
 # Create your views here.
-class RoomList(ListView):
-    model=Room
+def RoomListView(request):
+    room = Room.objects.all()[0]
+    room_categories = dict(room.ROOM_CATEGORIES)
+    room_values = room_categories.values()
+    room_list = []
+    for room_category in room_categories:
+        room = room_categories.get(room_category)
+        room_url = reverse('RoomDetailView',kwargs={'category': room_category})
+        room_list.append((room,room_url))
+    context = {
+        "room_list":room_list,
+    }
+    return render(request, './booking/room_view.html', context)
 
 class BookingList(ListView):
     model=Booking
+
+class RoomDetailView(View):
+    def get(self,request,*args,**kwargs):
+        category = self.kwargs.get('category',None)
+        form = AvailableForm()
+        room_list = Room.objects.filter(category=category)
+        if len(room_list) > 0:
+            room = room_list[0]
+            room_category = dict(room.ROOM_CATEGORIES).get(room.category, None)
+            context={
+                'room_category' : room_category,
+                'form': form,
+            }
+            return render(request, './booking/room_detail.html', context)
+        else:
+            return HttpResponse('Category doesnot exist!')
+
+    def post(self,request,*args,**kwargs):
+        category = self.kwargs.get('category',None)
+        room_list = Room.objects.filter(category=category)
+        form = AvailableForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+
+        available_rooms = []
+        for room in room_list:
+            if check_available(room,data['check_in'],data['check_out']):
+                available_rooms.append(room)
+        
+        if len(available_rooms) > 0 :
+            room = available_rooms[0]
+            booking = Booking.objects.create(
+                user = self.request.user,
+                room = room,
+                check_in = data['check_in'],
+                check_out = data['check_out']
+            ) 
+            booking.save
+            return HttpResponse(booking)
+        
+        else:
+            return HttpResponse('This category of rooms is booked! Try a different one!')
 
 class BookingView(FormView):
     form_class = AvailableForm
